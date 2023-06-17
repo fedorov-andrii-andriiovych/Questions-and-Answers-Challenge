6b8699c3c10736f071d73fb62aea53f4de02f594
Andrii Fedorov <fedorov.andrii.andriiovych@gmail.com>
2023-06-17 08:32:54 +0300

    add decoding string to response

--- app/src/main/java/com/fedorov/andrii/andriiovych/qachallenge/repositories/RetrofitNetworkRepositoryImpl.kt
+++ app/src/main/java/com/fedorov/andrii/andriiovych/qachallenge/repositories/RetrofitNetworkRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.fedorov.andrii.andriiovych.qachallenge.repositories
 
+import android.text.Html
 import android.util.Log
 import com.fedorov.andrii.andriiovych.qachallenge.model.QuestionModel
 import com.fedorov.andrii.andriiovych.qachallenge.network.QuestionServices
@@ -10,18 +11,23 @@ class RetrofitNetworkRepositoryImpl(private val questionServices: QuestionServic
 
     override suspend fun getNewQuestion(category: Int,difficulty: String ,type: String): QuestionModel {
         val questionResponse = questionServices.getNewQuestion(category = category, difficulty = difficulty, type = type).results[0]
+        val incorrectAnswers = questionResponse.incorrectAnswers.map { decodeHtmlString(it) }
+        val correctAnswer = decodeHtmlString(questionResponse.correctAnswer)
         val answers = mutableListOf<String>()
-        answers.addAll(questionResponse.incorrectAnswers)
-        answers.add(questionResponse.correctAnswer)
+        answers.addAll(incorrectAnswers)
+        answers.add(correctAnswer)
         answers.shuffle()
         return QuestionModel(
             category = questionResponse.category,
             type = questionResponse.type,
             difficulty = questionResponse.difficulty,
-            question = questionResponse.question,
-            correct_answer = questionResponse.correctAnswer,
-            incorrect_answers = questionResponse.incorrectAnswers,
+            question = decodeHtmlString(questionResponse.question),
+            correct_answer = correctAnswer,
+            incorrect_answers = incorrectAnswers,
             answers = answers
         )
+    }
+    fun decodeHtmlString(htmlString: String): String {
+        return Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY).toString()
     }
 }

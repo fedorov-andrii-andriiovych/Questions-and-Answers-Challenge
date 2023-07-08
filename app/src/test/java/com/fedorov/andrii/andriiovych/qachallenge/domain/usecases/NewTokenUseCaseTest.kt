40e929d330072e951b88aa64cedd9bf4f9f3ff1f
Andrii Fedorov <fedorov.andrii.andriiovych@gmail.com>
2023-07-08 11:33:06 +0300

    added test NewTokenUseCaseTest, return the same data

--- app/src/test/java/com/fedorov/andrii/andriiovych/qachallenge/domain/usecases/NewTokenUseCaseTest.kt
+++ app/src/test/java/com/fedorov/andrii/andriiovych/qachallenge/domain/usecases/NewTokenUseCaseTest.kt
@@ -1,4 +1,31 @@
 package com.fedorov.andrii.andriiovych.qachallenge.domain.usecases
 
+import com.fedorov.andrii.andriiovych.qachallenge.domain.repositories.NetworkRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
 class NewTokenUseCaseTest {
+
+    companion object{
+        private const val TRUE = true
+
+    }
+
+    private val networkRepository = mock<NetworkRepository>()
+
+    @Test
+    fun `should return the same data from repository`()  = runBlocking {
+
+        val testData = TRUE
+        Mockito.`when`(networkRepository.getNewToken()).thenReturn(testData)
+
+        val useCase = NewTokenUseCase(networkRepository = networkRepository)
+        val actual = useCase.getNewToken()
+        val expected = TRUE
+
+        Assertions.assertEquals(actual, expected)
+    }
 }
